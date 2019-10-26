68cf52639a0a9b31ed8c3a6ab94da67f8cbe7239
Breno Cunha Queiroz <breno1423@gmail.com>
2019-10-26 19:53:30 -0300

    Feat: Tutorial1

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # OpenGL-Tutorial
-Tutorial básico para começar a utilizar o OpenGL com C++
+
+## Introdução
+Tutorial básico para aprender como desenhar e manipular polígonos e linhas 2D utilizando OpenGL e C++.
+
+_Obs: Este tutorial é direto e não se aprofunda em conceitos do OpenGL._
+
+Alguns dos tópicos que são abordados:
+- Desenhar basic shapes (triangulo, retangulo, círculo)
+- Como mover os objetos 3D
+- Como receber cliques da tela
+
+
+## Tutoriais
+- **1-PrintBasicShapes** - Como desenhar triângulos, retângulos e círculos
+<p align="center">
+<img src="https://github.com/Brenocq/OpenGL-Tutorial/blob/master/img/Tutorial1.png?raw=true" height="100">
+</p>
+
+- **2-Animation**(TODO) - Como criar animações de objetos
+- **3-MouseClick**(TODO) - Como receber os dados do mouse na janela
+- **4-MenuWindow**(TODO) - Como criar um menu de seleção
+
+_(Se tiver mais sujestões/dúvidas só abrir um Issue aqui no Github)_
+
+## Instalação
+
+```
+$ sudo apt-get install mesa-common-dev
+$ sudo apt-get install freeglut3-dev
+```
+
+## Execução
+
+```
+$ cd OpenGL-Tutorial/tutoriais
+$ make clean
+$ make
+```
+
+Para executar o tutorial 1: `/run1`.<br>
+Para executar o tutorial 2: `/run2`.<br>
+(...)
+
+### Alguns projetos que utilizam OpenGL
+[ComputationalGeometry](https://github.com/Brenocq/ComputationalGeometryOpenGL)
+<p align="center">
+<img src="https://github.com/Brenocq/OpenGL-Tutorial/blob/master/img/CompGeometry.png?raw=true" height="200">
+</p>
+
+
+[**ObstacleAvoidanceOpenGL**](https://github.com/Brenocq/EvolutiveSystemOpenGL-ObstacleAvoidance)
+<p align="center">
+<img src="https://github.com/Brenocq/EvolutiveSystemOpenGL-ObstacleAvoidance/blob/master/img/gifReleasev1.0.gif?raw=true" height="200">
+</p>
+
+_Se você criou um projeto legal a partir desse tutorial e quer adicionar aqui, manda um pull request! :D_
---
+++ tutoriais/1-PrintBasicShapes.cpp
@@ -0,0 +1,130 @@
+#include <GL/glut.h>
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+#define windowWidth 600
+#define windowHeight 600
+
+// Função principal para desenhar
+void draw();
+
+// Funções para desenhar polígonos
+void retangle(float x, float y, float w, float h);
+void triangle(float x, float y, float h, float b);
+void circle(float x, float y, float radius);
+void strangePolygon(float x, float y);
+void anotherStrangePolygon(float x, float y);
+
+int main(int argc, char** argv){
+  //----- Criar janela -----//
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_RGB);
+  glutInitWindowSize(windowWidth, windowHeight);
+  glutInitWindowPosition(0, 0);
+  glutCreateWindow("Tutorial 1 - Print Basic Shapes");
+  glClearColor(1.0, 1.0, 1.0, 1.0);// Limpa a tela (red, green, blue, alpha)
+  glutDisplayFunc(draw);// Define qual função irá desenhar
+  glutMainLoop();
+
+  return 0;
+}
+
+void draw(){
+  // Importante: Somente será desenhado o que estiver entre glClear e glEnd
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  // A tela padrão do OpenGL tem suas coordenadas X e Y de -1 até 1
+  // Coordenada (-1 -1) é o canto inferior esquerdo
+  // Coordenada (-1 1) é o canto superior esquerdo
+  // Coordenada (0 0) é o centro da tela
+
+  retangle(0, 0, 0.1, 0.2);
+  triangle(0.5, 0.5, 0.4, 0.5);
+  circle(-0.5, 0.5, 0.1);
+  strangePolygon(-0.5, -0.5);
+  anotherStrangePolygon(0.5, -0.5);
+
+  glutSwapBuffers();
+}
+
+//------------------------------//
+//--------- RECTANGLE ----------//
+//------------------------------//
+
+void retangle(float x, float y, float w, float h){
+  // Vai desenhar um polígono de 4 vértices
+
+  glColor3f(0, 0, 1);// Define que será desenhar um objeto azul
+  glBegin(GL_POLYGON);// Fala para o OpenGL que os próximos pontos serão para desenhar um polígono
+
+  // Adicionada cada vértice do retângulo
+  glVertex2d( x-w/2, y-h/2);
+  glVertex2d( x-w/2, y+h/2);
+  glVertex2d( x+w/2, y+h/2);
+  glVertex2d( x+w/2, y-h/2);
+
+  glEnd();// Fala para o OpenGL que terminou de enviar os vértices do polígono
+}
+
+//------------------------------//
+//---------- TRIANGLE ----------//
+//------------------------------//
+
+void triangle(float x, float y, float h, float b){
+  // O mais polígono mais trivial: 3 vertíces
+
+  glColor3f(1, 0, 1);// Define que será desenhar um objeto roxo
+  glBegin(GL_POLYGON);
+
+  // Adicionada cada vértice do triângulo
+  glVertex2d( x-b/2, y-h/2);
+  glVertex2d( x+b/2, y-h/2);
+  glVertex2d( x, y+h/2);
+
+  glEnd();// Fala para o OpenGL que terminou de enviar os vértices do polígono
+}
+
+//------------------------------//
+//----------- CIRCLE -----------//
+//------------------------------//
+
+void circle(float x, float y, float radius){
+  // Um circulo é desenhado como um polígono de muitos pontos
+
+  glColor3f(0, 1, 0);// Define que será desenhar um objeto verde
+  glBegin(GL_POLYGON);
+  for (int i = 0; i < 360; i+=5) {
+    // Adicionada cada vértice 2D do circulo
+    glVertex2d( radius*cos(i/180.0*M_PI) + x, radius*sin(i/180.0*M_PI) + y);
+  }
+  glEnd();
+}
+
+//------------------------------//
+//----------- STRANGE ----------//
+//------------------------------//
+
+void strangePolygon(float x, float y){
+  glColor3f(0, 0, 0);// Define que será desenhar um objeto preto
+
+  glBegin(GL_POLYGON);
+  for (int i = 0; i < 360; i+=10) {
+    glVertex2d( 0.2*cos(i/180.0*M_PI) + x + cos(i)*0.15,  0.2*sin(i/180.0*M_PI) + y + sin(i)*0.15);
+  }
+  glEnd();
+
+}
+
+void anotherStrangePolygon(float x, float y){
+  glColor3f(1, 1, 0);// Define que será desenhar um objeto amarelo
+
+  glBegin(GL_POLYGON);
+  for (int i = 0; i < 360; i+=10) {
+    glVertex2d( 0.3*cos(i/180.0*M_PI) + x,  0.3*sin(i/180.0*M_PI) + y);
+    i+=10;
+    glVertex2d( 0.1*cos(i/180.0*M_PI) + x,  0.1*sin(i/180.0*M_PI) + y);
+  }
+  glEnd();
+}
